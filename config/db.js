5f00a4e3b2afd903741b259bf65853deb460e987
akghanekar000 <akghanekar000@gmail.com>
2025-08-22 22:13:49 +0530

    db newest 2nd time

--- config/db.js
+++ config/db.js
@@ -1,18 +1,13 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
 
-const connectDB = async () => {
+export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "erpdb", // forces DB name
     });
-    console.log("✅ MongoDB connected successfully!");
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("❌ MongoDB connection error:", error);
+    console.error(`❌ Error: ${error.message}`);
     process.exit(1);
   }
 };
-
-export default connectDB;
