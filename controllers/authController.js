0d231016663b7259fdac47e443bf535a3371021b
akghanekar000 <akghanekar000@gmail.com>
2025-09-08 22:06:36 +0530

    some changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
-import User from "../models/userModel.js";
+import User from "../models/User.js";
 
 // Helpers
 const generateAccessToken = (id) => {
